b6e0d9d3027c5317ceb46e1e2c4e5ff59aa8ef84
Yiran Zhang <zhang.yiran@northeastern.edu>
2023-03-21 15:53:08 -0700

    1

--- terraform/module/network.tf
+++ terraform/module/network.tf
@@ -203,53 +203,6 @@ resource "aws_db_instance" "zyrnj" {
   }
 }
 
-data "aws_iam_policy_document" "s3_policy" {
-  statement {
-    effect  = "Allow"
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:ListBucket",
-      "S3:DeleteObject"
-    ]
-
-    resources = [
-      "arn:aws:s3:::${aws_s3_bucket.zyrnj.bucket}",
-      "arn:aws:s3:::${aws_s3_bucket.zyrnj.bucket}/*"
-    ]
-  }
-}
-
-resource "aws_iam_policy" "s3_access_policy" {
-  name   = "s3-access-policy"
-  policy = data.aws_iam_policy_document.s3_policy.json
-}
-
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "example_attachment" {
-  policy_arn = aws_iam_policy.s3_access_policy.arn
-  role = aws_iam_role.ec2_role.name
-}
-
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "example-instance-profile"
-  role = aws_iam_role.ec2_role.name
-}
 
 resource "aws_instance" "web" {
   ami           = var.dev-ami
